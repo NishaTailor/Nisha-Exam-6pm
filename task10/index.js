16a9b4bb9dad5b5af82622a7046694434e261156
Nisha Tailor <tailornisha26@gmail.com>
2024-07-30 16:06:16 +0530

    Nisha Exam 6pm

--- task10/index.js
+++ task10/index.js
@@ -2,7 +2,6 @@
 let array=[1, 2, 4, 5, 6];
 let n=array.length
 
-let sum=0;
 for(let i=0; i<n; i++){
     n*(n+1)/2.
 }
--- task9/index.js
+++ task9/index.js
@@ -1,1 +1,4 @@
-// Check if Array is Sorted
+// Write a function to check if an array is sorted in ascending order.
+let arr = [5,3,4,2,1]
+arr.sort((a, b) => a - b)
+console.log(arr);
